987999a352a296e5894d9fee7d19cc7175807b32
Wh1spr <wh1spr.code@gmail.com>
2018-04-29 10:21:20 +0200

    First attempt at variant2
    Gets ConcurrentModificationException

--- src/main/algorithms/Variant2Alg.java
+++ src/main/algorithms/Variant2Alg.java
@@ -4,6 +4,7 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
 
 public class Variant2Alg extends AbstractAlg {
@@ -30,11 +31,13 @@ public class Variant2Alg extends AbstractAlg {
 	         return true;
 	    }
 	}; 
+	private List<Double[]> pcopy = null;
 	
 	//remove and add operation are the remove and add operations on status.
 	@Override
 	public void execute() {
 		status.clear();
+		pcopy = new ArrayList<Double[]>(this.getPoints());
 		if (this.getDim() != 2) {
 			this.getOut().println("Dit algoritme is niet geïmplementeerd voor dimensies niet gelijk aan 2. Gegeven dimensie: " + this.getDim());
 			this.getOut().flush();
@@ -44,11 +47,36 @@ public class Variant2Alg extends AbstractAlg {
 		
 		this.shortestDist = Double.POSITIVE_INFINITY;
 		this.closestPoints = new Double[2][this.getDim()];
-		
 		//algoritme	
+		Iterator<Double[]> iterx = Collections.unmodifiableList(pcopy).iterator();
+		Iterator<Double[]> itery = null;
+		Double[] x = null;
+		Double[] y = null;
+		while(iterx.hasNext()) { //doorlooplijn
+			x = iterx.next(); // point being checked
+			itery = Collections.unmodifiableCollection(status).iterator();
+			while(itery.hasNext()) {
+				//search, plus remove out of range x
+				y = itery.next();
+				if (y[0] < x[0] - this.shortestDist) status.remove(y);
+				if (Math.abs(x[1] - y[1]) > this.shortestDist) {
+					if (x[1] - y[1] < 0) break;
+					else continue;
+				}
+				
+				double dist = Math.pow(y[0] - x[0], 2) + Math.pow(y[1] - x[1], 2);
+				if (shortestDist > dist) {
+					closestPoints[0] = x;
+					closestPoints[1] = y;
+					shortestDist = dist;
+				}
+			}
+			status.add(x);
+		}
 	}
 	
 	private Double[] above(Double[] a) {
+		if(status.indexOf(a)>=0) return status.get(status.indexOf(a)+1);
 		status.add(a);
 		Double[] result = null;
 		try {
@@ -62,6 +90,7 @@ public class Variant2Alg extends AbstractAlg {
 	}
 
 	private Double[] under(Double[] a) {
+		if(status.indexOf(a)<=0) return status.get(status.indexOf(a)+1);
 		status.add(a);
 		Double[] result = null;
 		try {
