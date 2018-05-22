58366bb706b950c3bdbedd1aeac89e80cebea849
Wolfgang <wolfgang.mollmann@student.kuleuven.be>
2018-05-22 16:40:02 +0200

    ArrayList toegevoegd aan Variant1

--- src/main/algorithms/Variant1Alg.java
+++ src/main/algorithms/Variant1Alg.java
@@ -1,6 +1,7 @@
 package main.algorithms;
 
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Variant1Alg extends AbstractAlg {
@@ -13,19 +14,23 @@ public class Variant1Alg extends AbstractAlg {
 	public void execute() {
 		this.shortestDist = Double.POSITIVE_INFINITY;
 		this.closestPoints = new Double[2][this.getDim()];
+		ArrayList<Integer> kList = new ArrayList<Integer>(this.getPoints().size());
+		kList.add(0);
 		
 		// convenience
 		List<Double[]> p = this.getPoints();
 		
 		// Loop over all the points, we start from the second point so that
 		// we have at least 1 point in front of it, and thus we have an initial distance.
-		for (int i = 1; i < this.getPoints().size(); i++) { 
+		for (int i = 1; i < this.getPoints().size(); i++) {
+			int counter = 0;
 			for (int j = i-1; j >= 0; j--) {
 				if (p.get(i)[0] - p.get(j)[0] > Math.sqrt(shortestDist)) {
 					break;
 				}
 				// calc dist and check
 				// copied from SimpleAlg.java
+				counter++;
 				double currentDist = this.calcDist(p.get(i), p.get(j)); // This is most intensive operation
 				if (shortestDist > currentDist) {
 					closestPoints[0] = this.getPoints().get(i);
@@ -33,7 +38,7 @@ public class Variant1Alg extends AbstractAlg {
 					shortestDist = currentDist;
 				}
 				
-			}
+			} kList.add(counter);
 			
 		}
 		
