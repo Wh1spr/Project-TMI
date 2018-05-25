23b48588bdff07042758cf0df97a1a29563029a0
Wh1spr <wh1spr.code@gmail.com>
2018-05-25 09:59:58 +0200

    added data3 code

--- src/main/algorithms/Data.java
+++ src/main/algorithms/Data.java
@@ -24,6 +24,8 @@ public class Data {
 		System.out.println("Data 1 done");
 		data2();
 		System.out.println("Data 2 done");
+//		data3();
+//		System.out.println("Data 3 done");
 		
 	}
 	
@@ -95,7 +97,7 @@ public class Data {
 	private static void data2() {
 		PrintWriter out = null;
 		try {
-			out = new PrintWriter(new BufferedWriter (new FileWriter("data2.csv")));
+			out = new PrintWriter(new BufferedWriter (new FileWriter("data6.csv")));
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.exit(1);
@@ -107,22 +109,22 @@ public class Data {
 		out.print("#Points,Dimension,sorttime,ExecBruteNano,ExecVar1Nano,Kmax,Kavg\n");
 		
 		List<Double[]> p = null;
-		for (int dim = 2; dim < 30; dim++) {
+		for (int dim = 2; dim < 45; dim++) {
 			writeRand = false;
-			p = makeRandom(dim, 2500, out);
+			p = makeRandom(dim, 300, out);
 			a = new Variant1Alg(p, dim, null);
 			b = new SimpleAlg(p,dim,null);
 			
-			out.print("2500,"+dim + ",");
+			out.print("200,"+dim + ",");
 			
 			double kmaxavg = 0;
 			double kavgavg = 0;
 			
 			writeRand = true;
 			long start = System.nanoTime();
-			for(int execs = 0; execs < 10; execs++) {
+			for(int execs = 0; execs < 5; execs++) {
 				long cnstart = System.nanoTime();
-				p = makeRandom(dim, 2500, out);
+				p = makeRandom(dim, 300, out);
 				b.setPoints(p);
 				b.setDim(dim);
 				long cnstop = System.nanoTime();
@@ -131,12 +133,12 @@ public class Data {
 			}
 			long end = System.nanoTime();
 			
-			out.print((int)(((double) end-start)/10) + ",");
+			out.print((int)(((double) end-start)/5) + ",");
 			
 			start = System.nanoTime();
-			for(int execs = 0; execs < 50; execs++) {
+			for(int execs = 0; execs < 10; execs++) {
 				long cnstart = System.nanoTime();
-				p = makeRandom(dim, 2500, out);
+				p = makeRandom(dim, 300, out);
 				a.setPoints(p);
 				a.setDim(dim);
 				long cnstop = System.nanoTime();
@@ -147,13 +149,66 @@ public class Data {
 			}
 			end = System.nanoTime();
 			
-			kmaxavg = kmaxavg / 50d;
-			kavgavg = kavgavg / 50d;
+			kmaxavg = kmaxavg / 10d;
+			kavgavg = kavgavg / 10d;
 			
-			out.print((int)(((double) end-start)/50) + ",");
+			out.print((int)(((double) end-start)/10) + ",");
 			out.print(String.format("%12f,%12f\n", kmaxavg, kavgavg));
 			
 			System.out.println("current dim: " + dim);
+		}
+		
+		out.flush();
+		out.close();
+	}
+	
+	private static void data3() {
+		PrintWriter out = null;
+		try {
+			out = new PrintWriter(new BufferedWriter (new FileWriter("data3.csv")));
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+		Variant2Alg a = null;
+		
+		out.print("#Points,Dimension,sorttime,ExecVar2Nano,Kmax,Kavg\n");
+		
+		List<Double[]> p = null;
+		for (int dim = 2; dim < 30; dim++) {
+			writeRand = false;
+			p = makeRandom(dim, 2500, out);
+			a = new Variant2Alg(p, dim, null);
+			
+			out.print("2500,"+dim + ",");
+			
+			double kmaxavg = 0;
+			double kavgavg = 0;
+			
+			writeRand = true;
+			long start = System.nanoTime();
+			for(int execs = 0; execs < 20; execs++) {
+				long cnstart = System.nanoTime();
+				p = makeRandom(dim, 2500, out);
+				a.setPoints(p);
+				a.setDim(dim);
+				long cnstop = System.nanoTime();
+				start += cnstop-cnstart;
+				a.execute();
+				kmaxavg += a.getkmax();
+				kavgavg += a.getkavg();
+			}
+			long end = System.nanoTime();
+			
+			kmaxavg = kmaxavg / 20d;
+			kavgavg = kavgavg / 20d;
+			
+			out.print((int)(((double) end-start)/20) + ",");
+			out.print(String.format("%12f,%12f\n", kmaxavg, kavgavg));
+			
+			System.out.println("current dim: " + dim);
+			out.flush();
 		}
 		
 		out.flush();
--- src/main/algorithms/Variant2Alg.java
+++ src/main/algorithms/Variant2Alg.java
@@ -16,6 +16,20 @@ public class Variant2Alg extends AbstractAlg {
 		super(points, dim, outWriter);
 	}
 
+	private double kmax = 0d;
+	private double kavg = 0d;
+	private double ktotal = 0d;
+	
+	public double getkmax() {
+		return this.kmax;
+	}
+	public double getkavg() {
+		return this.kavg;
+	}
+	public double getktotal() {
+		return this.ktotal;
+	}
+	
 	Comparator<Double[]> compy = new Comparator<Double[]>() {
 		@Override
 		public int compare(Double[] o1, Double[] o2) {
@@ -30,6 +44,10 @@ public class Variant2Alg extends AbstractAlg {
 		this.shortestDist = 36d;
 		this.closestPoints = new Double[2][this.getDim()];
 		
+		this.kmax = 0d;
+		this.kavg = 0d;
+		this.ktotal = 0d;
+		double kthis = 0d;
 		// convenience
 		List<Double[]> p = this.getPoints();
 		
@@ -37,6 +55,7 @@ public class Variant2Alg extends AbstractAlg {
 		// Loop over all the points, we start from the second point so that
 		// we have at least 1 point in front of it, and thus we have an initial distance.
 		for (int i = 1; i < this.getPoints().size(); i++) { 
+			kthis = 0;
 			ysort.add(p.get(i-1));
 			
 			Double[] lo = new Double[] {p.get(i)[0], p.get(i)[1] - Math.sqrt(this.shortestDist)};
@@ -48,6 +67,8 @@ public class Variant2Alg extends AbstractAlg {
 					next = ysort.higher(next);
 					continue;
 				}
+				ktotal++;
+				kthis++;
 				double currentDist = this.calcDist(p.get(i), next); // This is most intensive operation
 				if (shortestDist > currentDist) {
 					closestPoints[0] = this.getPoints().get(i);
@@ -56,8 +77,10 @@ public class Variant2Alg extends AbstractAlg {
 				}
 				next = ysort.higher(next);
 			}
+			if (kthis > kmax) kmax = kthis;
 			
 		}
+		kavg = ktotal / ((double)(p.size()));
 		ysort.clear();
 	}
 	
